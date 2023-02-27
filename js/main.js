24ec60402a169debc57a50d340ba964219ad4cf2
FerrariMarc0 <marco1010@live.it>
2023-02-27 17:53:29 +0100

    carousel

--- js/main.js
+++ js/main.js
@@ -1,2 +1,34 @@
 'use strict'
 
+const images= ['01.webp','02.webp', '03.webp', '04.webp', '05.webp'];
+const carousel= document.querySelector('.slider');
+
+let divContent= '';
+let move= 0;
+
+for(let i = 0; i < images.length; i++){
+    divContent += `<div class="item"><img src="img/${images[i]}" alt="img"></div>`;
+}
+
+carousel.innerHTML += divContent;
+document.querySelector('.item').classList.add('show');
+
+const prev= document.querySelector('.up');
+const next= document.querySelector('.down');
+const items= document.querySelectorAll('.item');
+
+prev.addEventListener('click', function(){
+    if(0 < move){
+        items[move].classList.remove('show');
+        move--;
+        items[move].classList.add('show');
+    }
+});
+next.addEventListener('click', function(){
+    
+    if(move < items.length - 1){
+        items[move].classList.remove('show');
+        move++;
+        items[move].classList.add('show');
+    }
+});
